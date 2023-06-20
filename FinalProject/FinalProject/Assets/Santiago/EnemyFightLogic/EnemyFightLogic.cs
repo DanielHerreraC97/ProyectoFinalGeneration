ba7b29ebb55aa21f0cf0696bee0f86faad43e184
Santiago Acosta <acosta.robayo27@gmail.com>
2023-06-20 09:49:59 -0500

    fightLogic2

--- FinalProject/FinalProject/Assets/Santiago/EnemyFightLogic/EnemyFightLogic.cs
+++ FinalProject/FinalProject/Assets/Santiago/EnemyFightLogic/EnemyFightLogic.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class EnemyFightLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int enemyEnergy;
+    private playerGridMovement _playerGridMovement;
+    public TMP_Text energyCount;
+
+    private void Start()
     {
-        
+        _playerGridMovement = GameObject.FindWithTag("Player").GetComponent<playerGridMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        energyCount.text = enemyEnergy.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (_playerGridMovement.recall > enemyEnergy)
+            {
+                enemyEnergy -= _playerGridMovement.recall;
+                Destroy(gameObject,5f);
+            }
+
+            if (_playerGridMovement.recall < enemyEnergy)
+            {
+                Destroy(other.gameObject,5f);
+            }
+        }
     }
 }
