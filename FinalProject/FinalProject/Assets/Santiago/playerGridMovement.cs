ef871595a48ae5857b38f71a635ad972f7e2977f
Santiago Acosta <acosta.robayo27@gmail.com>
2023-06-21 10:05:06 -0500

    clean code

--- FinalProject/FinalProject/Assets/Santiago/playerGridMovement.cs
+++ FinalProject/FinalProject/Assets/Santiago/playerGridMovement.cs
@@ -7,55 +7,43 @@ using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 using TMPro;
 using Random = UnityEngine.Random;
-
 public class playerGridMovement : MonoBehaviour
 {
     [SerializeField] private Tilemap floor;
     [SerializeField] private Tilemap walls;
     private PlayerController controls;
-
-    public float penaltyTime;
-
     public UnityEvent moveEnemies;
-    
     //dice parameters 
     public int recall;
     private Dice _dice;
-    
     //penalty
     public float timer;
     public float resetTimer;
     public TMP_Text timerUI;
     public bool stopTimer;
     public Transform[] randomSpots;
-    
     private void Awake()
     {
         controls = new PlayerController();
     }
-
     private void OnEnable()
     {
         controls.Enable();
     }
-
     private void OnDisable()
     {
         controls.Disable();
     }
-
     void Start()
     {
         controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
        _dice = GameObject.FindWithTag("Dice").GetComponent<Dice>();
        stopTimer = false;
     }
-
     private void Update()
     {
         recall = _dice.finalSide;
     }
-
     private void Move(Vector2 direction)
     {
         if (PauseManager.paused) return;
@@ -67,7 +55,6 @@ public class playerGridMovement : MonoBehaviour
             timer = resetTimer;
         }
     }
-
     private bool CanMove(Vector2 direction)
     {
         Vector3Int gridPosition = floor.WorldToCell(transform.position + (Vector3)direction);
@@ -92,15 +79,11 @@ public class playerGridMovement : MonoBehaviour
                 Punishment();
             }
         }
-
-       
     }
-
     public void Punishment()
     {
         transform.position = randomSpots[Random.Range(0, 3)].position;
             timer = resetTimer;
             Debug.Log("punishment!");
     }
-
 }
