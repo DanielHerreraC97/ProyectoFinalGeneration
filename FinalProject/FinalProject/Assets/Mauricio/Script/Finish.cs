6e0303a3dd09c519c38333e9350d6bb29ac3e1ff
Mauricio <94552142+MMau55@users.noreply.github.com>
2023-07-05 05:40:54 -0500

    Update

--- FinalProject/FinalProject/Assets/Mauricio/Script/Finish.cs
+++ FinalProject/FinalProject/Assets/Mauricio/Script/Finish.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
+    public static string lastSceneName;
+
+    private void Start()
+    {
+    }
+
     public void RetryGame()
     {
         AudioManager.Instance.PlaySFX("Botones");
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(lastSceneName);
+    }
+
+    public void Base()
+    {
+        AudioManager.Instance.PlaySFX("Botones");
+        SceneManager.LoadScene("BaseLevel");
     }
     public void MainMenu()
     {
         AudioManager.Instance.PlaySFX("Botones");
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("MainMenu");
     }
 }
